adb61692de63df045301e978051b689a5bc08ee8
Oleg Gusev <magentoassistance@gmail.com>
2017-10-27 19:39:56 +0300

    HOME WORK: UNIT 3: 3.3.1. In the core files, find and print out the layout XML for the product view page.

---
+++ app/code/Training/Test/Framework/View/Layout.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * CreateStores Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@createstores.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * If you wish to customize it for your
+ * needs please refer to http://www.createstores.com for more information.
+ *
+ * @creator     Gusev Oleg, Magento certificated Developer Plus, magentoassistance@gmail.com
+ * @certificate http://www.magentocommerce.com/certification/directory/dev/973862/
+ *
+ * Created by
+ * User: oleg
+ * Date: 27.10.17 Time: 18:46
+ * @category    CreateStores
+ * @package
+ * @copyright   Copyright (c) 2017 CreateStores Inc. (http://www.createstores.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Training\Test\Framework\View;
+
+
+/**
+ * Class Layout
+ * @package Training\Framework\View
+ */
+class Layout extends \Magento\Framework\View\Layout
+{
+	/**
+	 * Layout xml generation
+	 *
+	 * result: http://take.ms/IRAou
+	 *
+	 * @return $this
+	 */
+	public function generateXml()
+	{
+		parent::generateXml();
+
+		$this->logger->info(var_export($this->getXmlString(), true));
+
+
+		return $this;
+	}
+}
