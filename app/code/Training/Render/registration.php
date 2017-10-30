0b6ab23ff5ed6ce630b6c61176da4cec16029105
Oleg Gusev <magentoassistance@gmail.com>
2017-10-30 21:24:12 +0200

    HOME WORK: UNIT 3: 3.8.1. Add a default.xml layout file to the Training_Render module. + comment prev tasks

--- app/code/Training/Render/registration.php
+++ app/code/Training/Render/registration.php
@@ -22,44 +22,14 @@
  *
  * Created by
  * User: oleg
- * Date: 30.10.17 Time: 19:16
+ * Date: 30.10.17 Time: 20:48
  * @category    CreateStores
  * @package
  * @copyright   Copyright (c) 2017 CreateStores Inc. (http://www.createstores.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
-namespace Training\Test\Block\Catalog\Product\View;
-
-/**
- *
- * plugin Description
- * @package Training\Test\Block\Catalog\Product\View
- */
-class Description extends \Magento\Catalog\Block\Product\View\Description
-{
-	/**
-	 * result http://take.ms/5GR56
-	 *
-	 * @return \Magento\Catalog\Block\Product\View\Description
-	 */
-	protected function _beforeToHtml()
-	{
-		//$_product = $this->getProduct();
-		//$_product->setDescription('Test description from Training\Test\Block\Catalog\Product\View\Description');
-
-		return parent::_beforeToHtml();
-	}
-
-	/**
-	 * result: http://take.ms/ttDHk
-	 *
-	 * @return \Magento\Catalog\Block\Product\View\Description
-	 */
-	protected function _prepareLayout()
-	{
-		//$this->setTemplate('Training_Test::description.phtml');
-
-		return parent::_prepareLayout();
-	}
-}
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Training_Render',
+	__DIR__
+);
