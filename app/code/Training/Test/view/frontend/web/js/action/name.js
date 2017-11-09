ede850c65a3c677fa954ed94284a40ec9e9e44b3
Oleg Gusev <magentoassistance@gmail.com>
2017-11-10 01:33:24 +0200

    Unit 3.5 Reinforcement Exercise 3.5-2-1 Create an extension that alerts the full action name on every page.

---
+++ app/code/Training/Test/view/frontend/web/js/action/name.js
@@ -0,0 +1,12 @@
+define([
+    'jquery'
+    ],
+    function ($) {
+        'use strict';
+        /**
+         * result: http://take.ms/KRVLu
+         */
+    return function(config, element) {
+        alert('Full action: ' + config.full_action);
+    }
+});
